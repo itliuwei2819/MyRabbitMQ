18a114b26fa773eb031c30e6807eafa10ee44a7f
刘伟 <liuwei@zhuimengkeji.com>
2020-03-04 14:57:19 +0800

    交换机Fanout类型示例代码

---
+++ my-rabbitmq/src/main/java/com/toolv/myrabbitmq/exchange/type/fanout/producer/ProducerDemo.java
@@ -0,0 +1,40 @@
+package com.toolv.myrabbitmq.exchange.type.fanout.producer;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class ProducerDemo
+{
+	public static void main(String[] args) throws IOException, TimeoutException, InterruptedException
+	{
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		connectionFactory.setHost("192.168.219.101");
+		connectionFactory.setPort(5672);
+		connectionFactory.setVirtualHost("/");
+		connectionFactory.setUsername("admin");
+		connectionFactory.setPassword("admin");
+
+		Connection connection = connectionFactory.newConnection();
+		Channel channel = connection.createChannel();
+
+		String exchangeName = "fanout_exchange_name";
+		String routingKey = "must be non-null";
+
+		for (int i = 0; i < 10; i++)
+		{
+			String msg = "Hello RabbitMQ - " + i;
+			System.out.println("producer=>" + msg);
+			channel.basicPublish(exchangeName, routingKey, null, msg.getBytes(StandardCharsets.UTF_8));
+			TimeUnit.SECONDS.sleep(1);
+		}
+
+		channel.close();
+		connection.close();
+	}
+}
