cc90c100ab198f9825454ca2964a9f9e73d6ff6f
刘伟 <liuwei@zhuimengkeji.com>
2020-03-04 14:35:12 +0800

    交换机Topic类型示例代码

---
+++ my-rabbitmq/src/main/java/com/toolv/myrabbitmq/exchange/type/topic/consumer/TopicConsumer.java
@@ -0,0 +1,49 @@
+package com.toolv.myrabbitmq.exchange.type.topic.consumer;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.ShutdownSignalException;
+
+public class TopicConsumer implements Consumer
+{
+	@Override
+	public void handleConsumeOk(String consumerTag)
+	{
+
+	}
+
+	@Override
+	public void handleCancelOk(String consumerTag)
+	{
+
+	}
+
+	@Override
+	public void handleCancel(String consumerTag) throws IOException
+	{
+
+	}
+
+	@Override
+	public void handleShutdownSignal(String consumerTag, ShutdownSignalException sig)
+	{
+
+	}
+
+	@Override
+	public void handleRecoverOk(String consumerTag)
+	{
+
+	}
+
+	@Override
+	public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException
+	{
+		String s = new String(body, StandardCharsets.UTF_8);
+		System.out.println(s);
+	}
+}
